601c5df881903acc2c1c9d3b36fecdd190f52083
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-02 17:21:01 +0300

    added code using gulp 4

--- hard/gulpfile.js
+++ hard/gulpfile.js
@@ -1,25 +1,54 @@
 'use strict';
 
-const gulp = require('gulp'),
-  cssnano = require('gulp-cssnano'),
+//Code Gulp 4
+const { src, dest, watch } = require('gulp'),
   rename = require('gulp-rename'),
+  cssnano = require('gulp-cssnano'),
   browserSync = require('browser-sync').create();
+
+function miniCSS () {
+  return src('src/css/style.css')
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(dest('src/css'))
+}
+
+function BrowserSync () {
+  browserSync.init({
+    server: {
+        baseDir: "src"
+    }
+  });
+  watch("src/*.html").on('change', browserSync.reload);
+}
+
+exports.miniCSS = miniCSS;
+exports.BrowserSync = BrowserSync;
+
+
+
+//Code Gulp 3
+
+//const gulp = require('gulp'),
+//  cssnano = require('gulp-cssnano'),
+ // rename = require('gulp-rename'),
+//  browserSync = require('browser-sync').create();
 
 
 // Static server
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    server: {
-      baseDir: 'src'
-    }
-  });
-  gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+//gulp.task('browser-sync', () => {
+ // browserSync.init({
+//    server: {
+//      baseDir: 'src'
+//    }
+//  });
+//  gulp.watch("src/*.html").on('change', browserSync.reload);
+//});
 
 // Minifier CSS module + rename CSS
-gulp.task('Mini-CSS', () => {
-  return gulp.src('src/css/style.css')
-    .pipe(cssnano())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('src/css'))
-});
+//gulp.task('Mini-CSS', () => {
+//  return gulp.src('src/css/style.css')
+ //   .pipe(cssnano())
+ //   .pipe(rename({suffix: '.min'}))
+//    .pipe(gulp.dest('src/css'))
+//});
