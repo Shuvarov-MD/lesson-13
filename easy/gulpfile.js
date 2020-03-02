8542bce95a854a0a9018c269b56727508f1ab0a7
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-02 17:13:51 +0300

    added code using gulp 4

--- easy/gulpfile.js
+++ easy/gulpfile.js
@@ -1,15 +1,36 @@
 'use strict';
 
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+// Code Gulp 4
+const { watch } = require('gulp'),
+  browserSync = require('browser-sync').create();
+
+function BrowserSync() {
+  browserSync.init({
+    server: {
+        baseDir: "src"
+    }
+  });
+  watch("src/*.html").on('change', browserSync.reload);
+}
+
+exports.BrowserSync = BrowserSync;
+
+
+
+// Code Gulp 3
+
+//const gulp = require('gulp');
+//const browserSync = require('browser-sync').create();
 
 
 // Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'src'
-    }
-  });
-  gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+//gulp.task('browser-sync', function() {
+//  browserSync.init({
+//    server: {
+//      baseDir: 'src'
+//    }
+//  });
+//  gulp.watch("src/*.html").on('change', browserSync.reload);
+//});
+
+
