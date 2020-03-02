22333936c04f5da4094fe6412156c58263eac846
Mikhail Shuvarov <shyvarov@gmail.com>
2020-03-02 11:25:56 +0300

    Include gulp & make gulpfile.js

---
+++ easy/gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    }
+  });
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
